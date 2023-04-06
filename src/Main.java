24e595e378eaf7765755fd47268923ade1376023
Korneliya93 <korneliya311293@gmail.com>
2023-04-05 20:32:30 -0500

    forth commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("scooby dobbie");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+            System.out.println(i+1);
+            System.out.println(i+2);
+            System.out.println(i+3);
+
+        }
     }
 }
